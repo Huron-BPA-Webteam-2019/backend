cff99590cf1c2ed509f6b2f4e4f1a52167917f2c
Peter Stenger <peter.a.stenger@gmail.com>
2020-01-30 22:55:50 -0500

    test finished L not

--- server.py
+++ server.py
@@ -10,14 +10,19 @@ app = Flask(__name__)
 def ping():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return 'pong!'
-@app.route('/calculate')
-def calculate():
+@app.route('/price')
+def price():
     data = requests.get('https://api.myjson.com/bins/14s9u2').json()
     rooms = request.args.get("rooms")
     sqft = request.args.get("sqft")
     greenft = request.args.get("greenft")
-    resp = Response({'price': data['a'] * rooms + data['b'] * sqft + data['c'] * greenft})
+    val = calculate(rooms, sqft, greenft, data)
+    resp = Response(val)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+def calculate(rooms, sqft, greenft, data):
+    # Remove data parameter later, just for testinf
+    return data['a'] * rooms + data['b'] * sqft + data['c'] * greenft
 if __name__ == '__main__':
     app.run(port=5000)
