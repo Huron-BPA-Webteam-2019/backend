8922759e684aef37ccf07c14f43176ba26b3929b
Peter Stenger <peter.a.stenger@gmail.com>
2020-01-30 22:34:10 -0500

    getting closer

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import request
-
+import requests
+import json
 app = Flask(__name__)
 
 @app.route('/ping')
 @app.route('/')
 def ping():
     return 'pong!'
+@app.route('/calculate'):
+    response = requests.get('https://api.myjson.com/bins/14s9u2').json()
+    rooms = request.args.get("rooms")
+    sqft = request.args.get("sqft")
+    greenft = request.args.get("greenft")
+    return f'''
+    {response['a']}, {response['b']}, {response['c']}
+    rooms {rooms}
+    sqft {sqft}
+    greenft {greenft}
+    '''
 if __name__ == '__main__':
     app.run(port=5000)
