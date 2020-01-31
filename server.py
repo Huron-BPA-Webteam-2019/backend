c4db06009e17924d36907c452f88a632b5ea4aa4
Peter Stenger <peter.a.stenger@gmail.com>
2020-01-30 22:49:55 -0500

    test finished L not

--- server.py
+++ server.py
@@ -11,16 +11,12 @@ def ping():
     return 'pong!'
 @app.route('/calculate')
 def calculate():
-    response = requests.get('https://api.myjson.com/bins/14s9u2').json()
+    data = requests.get('https://api.myjson.com/bins/14s9u2').json()
     rooms = request.args.get("rooms")
     sqft = request.args.get("sqft")
     greenft = request.args.get("greenft")
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return f'''
-    {response['a']}, {response['b']}, {response['c']}
-    rooms {rooms}
-    sqft {sqft}
-    greenft {greenft}
-    '''
+    resp = {'price': data['a'] * rooms + data['b'] * sqft + data['c'] * greenft}
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 if __name__ == '__main__':
     app.run(port=5000)
